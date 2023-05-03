985b90074dd9ffd6f2171109e3e83c1578fad61d
guiprodigy <guiihprodigy@gmail.com>
2023-05-03 10:44:31 -0300

    primeirocommit

---
+++ Aula01/Models/ClientesModelView.cs
@@ -0,0 +1,10 @@
+﻿namespace Aula01.Models
+{
+    public class ClientesModelView
+    {
+        public string Nome { get; set; }
+        public string Valor { get; set; }
+        public string Quantidade { get; set; }
+        public long Id { get; set; }
+    }
+}
